bfc2725205448fefea7273c1c87ba104f7693656
Rashed Rifat <rmr557@nyu.edu>
2022-03-16 19:48:24 -0400

    experimenting with Ruby classes

--- hello.rb
+++ hello.rb
@@ -1,3 +1,15 @@
-puts "Hello World!\nPlease enter your name!"
-name = gets.chomp 
-puts "Hello #{name}!"
+class HelloWorld
+    def initialize(id, name)
+        @id = id
+        @name = name 
+    end 
+
+    def sayHello()
+        puts "Hello World!"
+        puts "My name is #@name"
+        puts "I am Machine-#@id"
+    end 
+end 
+
+jane = HelloWorld.new(12345, "Jane")
+jane.sayHello()
